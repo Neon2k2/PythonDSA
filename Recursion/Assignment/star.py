050df75d1c15a03f01644534868ac070d6177c00
Gaurish Ninnekar <44323737+Neon2k2@users.noreply.github.com>
2023-02-18 23:43:19 +0530

    Update star.py

--- Recursion/Assignment/star.py
+++ Recursion/Assignment/star.py
@@ -1,4 +1,11 @@
 # Given a string S,compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+# Constraints :
+# 0 <= |S| <= 1000
+# where |S| represents length of string S.
+# Sample Input 1 :
+# hello
+# Sample Output 1:
+# hel*lo
 
 def pairStar(a):
     if len(a) == 0:
